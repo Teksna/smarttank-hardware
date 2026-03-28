15326b54e811cfb2256f0171a679e80c02a88f97
TEKSNA <ashrafumair21@gmail.com>
2026-03-28 18:10:22 +0530

    tested with GET and POST requests

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,79 @@
 #include <RadioLib.h>
 #include "display/display.h"
 #include "ota/ota.h"
+#include <HTTPClient.h>
+const char* supabaseUrl = "https://yljggigahlagdihhycfj.supabase.co";
+const char* supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsamdnaWdhaGxhZ2RpaGh5Y2ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MDcwNTMsImV4cCI6MjA4ODI4MzA1M30.NeGRlQv-T-OGW4iqJPLV2T-2uPQxDNz0r9GHLAG8F-g";
+bool cloudSupply = false;
+bool fetchSupplyState() {
 
+    if (WiFi.status() != WL_CONNECTED) return false;
+
+    HTTPClient http;
+
+    String url = String(supabaseUrl) + 
+      "/rest/v1/iot_logs?select=supply&order=created_at.desc&limit=1";
+
+    http.begin(url);
+    http.addHeader("apikey", supabaseKey);
+    http.addHeader("Authorization", "Bearer " + String(supabaseKey));
+
+    int httpCode = http.GET();
+
+    if (httpCode == 200) {
+        String payload = http.getString();
+
+        // Simple parse (lightweight)
+        if (payload.indexOf("true") > 0) {
+            cloudSupply = true;
+        } else {
+            cloudSupply = false;
+        }
+
+        Serial.print("Cloud Supply: ");
+        Serial.println(cloudSupply);
+    } else {
+        Serial.print("Fetch Error: ");
+        Serial.println(httpCode);
+    }
+
+    http.end();
+    return cloudSupply;
+}
+void sendToSupabase(int tank, bool motor, int rssi) {
+
+    if (WiFi.status() != WL_CONNECTED) return;
+
+    HTTPClient http;
+
+    String url = String(supabaseUrl) + "/rest/v1/iot_logs";
+
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("apikey", supabaseKey);
+    http.addHeader("Authorization", "Bearer " + String(supabaseKey));
+    http.addHeader("Prefer", "return=minimal"); // ⚡ faster
+
+    // JSON payload (lightweight)
+    String payload = "{";
+    payload += "\"username\":\"tank1\",";
+    payload += "\"motor\":" + String(motor ? "true" : "false") + ",";
+    payload += "\"tank\":" + String(tank) + ",";
+    payload += "\"rssi\":" + String(rssi);
+    payload += "}";
+
+    int httpResponseCode = http.POST(payload);
+
+    if (httpResponseCode > 0) {
+        Serial.print("Supabase OK: ");
+        Serial.println(httpResponseCode);
+    } else {
+        Serial.print("Supabase Error: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end(); // free memory
+}
 // ---------------- WIFI ----------------
 const char* ssid = "Airtel_mohd_3792";
 const char* password = "Air@28347";
@@ -92,7 +164,13 @@ void setup() {
 
 // ---------------- LOOP ----------------
 void loop() {
-
+    unsigned long lastFetch = 0;
+    bool supplyState;
+    // 🔄 Fetch supply every 3 sec
+    if (millis() - lastFetch > 3000) {
+        supplyState = fetchSupplyState();
+        lastFetch = millis();
+    }
     LoRaPacket pkt = receiveLoRa();
 
     if (pkt.state == RADIOLIB_ERR_NONE) {
@@ -112,21 +190,29 @@ void loop() {
         Serial.print(" cm | Capacity: ");
         Serial.print(capacity);
         Serial.println(" %");
-
+        int rssi = radio.getRSSI();
+        Serial.print("RSSI: ");
+        Serial.print(rssi);
+        
         // ---------------- DISPLAY ----------------
-        displayStatus(capacity, radio.getRSSI());
+        displayStatus(capacity, rssi);
 
         // ---------------- MOTOR LOGIC ----------------
-        if (capacity < 20 && !motorState) {
+        if (capacity < 20 && !motorState && supplyState) {
             digitalWrite(motorPin, HIGH);
             motorState = true;
             Serial.println("Motor ON");
         }
-        else if (capacity > 90 && motorState) {
+        else if (capacity > 90 && motorState && !supplyState) {
             digitalWrite(motorPin, LOW);
             motorState = false;
             Serial.println("Motor OFF");
         }
+        unsigned long lastUpload = 0;
+        if (millis() - lastUpload > 5000) {  // every 5 sec
+        sendToSupabase(capacity, motorState, rssi);
+        lastUpload = millis();
+    }
     }
     else if (pkt.state == RADIOLIB_ERR_RX_TIMEOUT) {
         Serial.println("timeout");
@@ -138,6 +224,6 @@ void loop() {
         Serial.print("LoRa error: ");
         Serial.println(pkt.state);
     }
-
+    
     delay(100);
 }
