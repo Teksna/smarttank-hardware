3351425d35eee77555d00b1e063f5011c2ddb890
TEKSNA <ashrafumair21@gmail.com>
2026-04-05 15:49:27 +0530

    working tx module

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <RadioLib.h>
 
 // NSS, DIO0, RESET, DIO1
-SX1276 radio = new Module(5, 2, 14, 3);
+#define ss 5
+#define rst 14
+#define dio0 2
+#define dio1 3
+#define echoPin 27  // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 26 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance;
+SX1276 radio = new Module(ss, dio0, rst, dio1);
 
 int count = 0;
 
 void setup() {
+  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
   Serial.begin(9600);
 
   Serial.print("[SX1276] Initializing ... ");
@@ -29,12 +43,32 @@ void setup() {
     while (true);
   }
 }
+String dist()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  return (String)distance;
 
+}
 void loop() {
   Serial.print("Transmitting... ");
 
-  String str = "Hello #" + String(count++);
-  int state = radio.transmit(str);
+  // String str = "Hello #" + String(count++);
+  String capacity = dist();
+  Serial.print("Distance :"+capacity);
+  int state = radio.transmit(capacity);
 
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println("success!");
